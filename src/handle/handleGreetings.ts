ed4b2ae887fa8f209df48588c4a92ea3a873af19
ThoArrow <57243290+thoarrown@users.noreply.github.com>
2021-12-01 16:24:49 +0700

    fix undefine username

--- src/handle/handleGreetings.ts
+++ src/handle/handleGreetings.ts
@@ -3,8 +3,14 @@ import Context from '@/models/Context'
 export default async function handleGreetings(ctx: Context) {
   for (const member of ctx.update.message.new_chat_members) {
     await ctx.reply(
-      `Chào mừng ${member.username} đã đến với Gamefinity, xem hướng dẫn cho người mới [tại đây](https://t.me/gamefinitychannel/181) . `,
-      { parse_mode: 'Markdown', disable_web_page_preview: true,allow_sending_without_reply:false }
+      `Chào mừng ${
+        member.username || member.first_name + ' ' + member.last_name
+      } đã đến với Gamefinity, xem hướng dẫn cho người mới [tại đây](https://t.me/gamefinitychannel/181) . `,
+      {
+        parse_mode: 'Markdown',
+        disable_web_page_preview: true,
+        allow_sending_without_reply: false,
+      }
     )
   }
 }

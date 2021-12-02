636d02a474bec5d9d69a91a4517721f17a95a004
ThoArrow <57243290+thoarrown@users.noreply.github.com>
2021-12-02 08:10:06 +0700

    Fix last_name undefined

--- src/handle/handleGreetings.ts
+++ src/handle/handleGreetings.ts
@@ -4,8 +4,8 @@ export default async function handleGreetings(ctx: Context) {
   for (const member of ctx.update.message.new_chat_members) {
     await ctx.reply(
       `Chào mừng ${
-        member.username || member.first_name + ' ' + member.last_name
-      } đã đến với Gamefinity, xem hướng dẫn cho người mới [tại đây](https://t.me/gamefinitychannel/181) . `,
+        member.username || (member.first_name || '') + ' ' + (member.last_name || '')
+      } đã đến với Gamefinity, xem hướng dẫn cho người mới [tại đây](https://t.me/gamefinitychannel/181).`,
       {
         parse_mode: 'Markdown',
         disable_web_page_preview: true,

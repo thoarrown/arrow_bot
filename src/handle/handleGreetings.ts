2abc0623c0e5612e48692b5fc65f71b055c3fc8f
ThoArrow <57243290+thoarrown@users.noreply.github.com>
2021-12-03 12:08:41 +0700

    Fix escap msg

--- src/handle/handleGreetings.ts
+++ src/handle/handleGreetings.ts
@@ -1,11 +1,24 @@
 import Context from '@/models/Context'
 
 export default async function handleGreetings(ctx: Context) {
+
+  function toEscapeMSg(str: string): string {
+    return str
+      .replace(/_/gi, '\\_')
+      .replace(/-/gi, '\\-')
+      .replace('~', '\\~')
+      .replace(/`/gi, '\\`')
+      .replace(/\./g, '\\.')
+  }
+
   for (const member of ctx.update.message.new_chat_members) {
+    const name =
+      member.username ||
+      (member.first_name || '') + ' ' + (member.last_name || '')
     await ctx.reply(
-      `Chào mừng ${
-        member.username || (member.first_name || '') + ' ' + (member.last_name || '')
-      } đã đến với Gamefinity, xem hướng dẫn cho người mới [tại đây](https://t.me/gamefinitychannel/181).`,
+      `Chào mừng ${toEscapeMSg(
+        name
+      )} đã đến với Gamefinity, xem hướng dẫn cho người mới [tại đây](https://t.me/gamefinitychannel/181).`,
       {
         parse_mode: 'Markdown',
         disable_web_page_preview: true,
